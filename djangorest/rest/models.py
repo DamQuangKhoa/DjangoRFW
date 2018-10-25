5b0f69727265663ac72e256452b0fa30f9c83b65
Mind <15130077@st.hcmuaf.edu.vn>
2018-10-25 16:19:39 +0700

    Basic API- Detail View & List View

--- djangorest/rest/models.py
+++ djangorest/rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+class Post(models.Model):
+   title = models.CharField( max_length = 200)  
+   content = models.TextField() 
+   draft = models.BooleanField(default = False) 
+   read_time = models.IntegerField(default = 0) 
+   updated = models.DateTimeField( auto_now=True, auto_now_add=False)
+   created = models.DateTimeField( auto_now=False, auto_now_add=True)
+
+   class Meta:
+       ordering = ["-created","-updated"]
+    
+
+   def __str__(self):
+       return self.title 
+    
--- djangorest/rest/tests.py
+++ djangorest/rest/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+GET http://localhost:8000/api/v1/posts HTTP/1.1
+
+GET https://example.com/topics/1 HTTP/1.1
